d897165199d0e8392023fd33f3e38cf89edaa5fd
Pickra <picker10024@yahoo.com>
2013-09-22 16:14:28 -0400

    kiddy pool tests passing

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,24 @@
 /*global describe, it */
 'use strict';
 (function () {
-    describe('Git-Smack Chat App', function () {
-    	it('should should have an array called messages', function () {
-
+    describe('Git-Smack Chat', function () {
+    	it('should should have an an instance of MessageCollection messages', function () {
                     expect(messages !== undefined || null).to.equal(true);
-                });
+        });
 
+        it('should be using jQuery', function () {
+                    expect(jQuery() !== undefined || null).to.equal(true);
+        });
 
+        it('should have an instance of Parse Collection called MessageCollection', function () {
+                    expect(MessageCollection !== undefined || null).to.equal(true);
+
+        }); 
+
+        it('should have an instance of Parse Object called Message', function () {
+                    expect(Message !== undefined || null).to.equal(true);
+
+        });
 
 
         // describe('unit test', function () {
