86f642d058e6a83e930450a1c31e0c4a84f624d3
Pickra <picker10024@yahoo.com>
2013-09-22 15:58:17 -0400

    1, 1  passing test, ha ha haaaaaaaaa

--- test/spec/test.js
+++ test/spec/test.js
@@ -2,7 +2,15 @@
 'use strict';
 (function () {
     describe('Git-Smack Chat App', function () {
-    	// describe('unit test', function () {
+    	it('should should have an array called messages', function () {
+
+                    expect(messages !== undefined || null).to.equal(true);
+                });
+
+
+
+
+        // describe('unit test', function () {
     	// 	it('should confirm obj instance exists', function () {
      //            $('#submit-btn').click()
     	// 		expect(message).to.equal(true);
