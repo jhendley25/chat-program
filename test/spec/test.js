6a4ac21fdb69b70e2d547fc467ea7e4b8606f4b4
Pickra <picker10024@yahoo.com>
2013-09-22 21:53:20 -0400

    integrtation test failing

--- test/spec/test.js
+++ test/spec/test.js
@@ -2,7 +2,7 @@
 'use strict';
 (function () {
     describe('Git-Smack Chat', function () {
-    	it('should should have an an instance of MessageCollection messages', function () {
+    	it('should have an instance of MessageCollection messages', function () {
                     expect(messages !== undefined || null).to.equal(true);
         });
 
@@ -19,15 +19,43 @@
                     expect(Message !== undefined || null).to.equal(true);
 
         });
-
-
-        // describe('unit test', function () {
-    	// 	it('should confirm obj instance exists', function () {
-     //            $('#submit-btn').click()
-    	// 		expect(message).to.equal(true);
-    	// 	})
-    	// })
-
-    
     })
+
+    describe('uses Parse as a backend server', function(){
+
+                this.timeout(15000);
+     
+                it('by sending a new message to Parse when you hit the submit button', function(done){
+                
+                var result;
+     
+                var form = $('.form-control');
+                var randomMessage = 'A Test Message #'+ Math.floor(Math.random()*10000000)  
+                form.val(randomMessage);
+     
+                $('.submit-btn').click()
+        
+                    setTimeout((function(){
+    
+                    var query = new Parse.Query(Message);
+                    query.equalTo("message", randomMessage);
+                    query.find({
+                        success: function(results) {
+                            result = results[0];
+
+                            console.log(result);
+
+                            expect(result.get('message')).to.equal(randomMessage);
+                            done();
+                        },
+                        error: function(error) {
+                            done(error.description)
+                        }
+                    });
+                 
+                }), 2000);
+            }); 
+                 
+        });
+        
 })();
